863f9f925641271893ac95cda2d34bdd0583c0e4
Mark Bell <mark00bell@googlemail.com>
2019-11-06 19:05:09 +0000

    Used black.

--- euler202/soln.py
+++ euler202/soln.py
@@ -1,21 +1,32 @@
 from sage.all import factor, prod, Subsets
 
+
 def get_way_count(surface_count):
     n = (surface_count + 3) // 2
     factors = [x for x, _ in factor(n)]
-    
+
     def f(x):
         first_k = (x * (-x % 3) - 2) // 3
         last_k = (n - 2) // 3
         last_k = last_k - (last_k - first_k) % x
         return (last_k - first_k) // x + 1
 
-    return (n - 2) // 3 + 1 + sum( (+1 if len(subset) % 2 == 0 else -1) * f(prod(subset)) for subset in Subsets(factors) if subset)
+    return (
+        (n - 2) // 3
+        + 1
+        + sum(
+            (+1 if len(subset) % 2 == 0 else -1) * f(prod(subset))
+            for subset in Subsets(factors)
+            if subset
+        )
+    )
+
 
 def main():
     print(get_way_count(11))
     print(get_way_count(1000001))
     print(get_way_count(12017639147))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='euler202',
-    version='0.0.1',
+    version='0.0.3',
     description='Solves Euler problem 202',
     author='Mark Bell',
     author_email='mcbell@illinois.edu',
